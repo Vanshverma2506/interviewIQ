e6ac3bacd4d3bc6852c182aef0d8787aaa84444f
vansh verma <vanshverma8937@gmail.com>
2026-03-17 23:04:05 +0530

    create interviewmodel

---
+++ backend/models/interview.model.js
@@ -0,0 +1,49 @@
+import mongoose, { Schema } from "mongoose";
+
+const questionSchema = new mongoose.Schema({
+  question: String,
+  difficulty: String,
+  timeLimit: String,
+  answer: String,
+  feedback: String,
+  score: { type: Number, default: 0 },
+  confidence: { type: Number, default: 0 },
+  communication: { type: Number, default: 0 },
+  correctness: { type: Number, default: 0 },
+});
+
+const interviewSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    expreince: {
+      type: String,
+      reuqired: true,
+    },
+    mode: {
+      type: String,
+      enum: ["HR,Technical"],
+      reuqired: true,
+    },
+    resumeText: {
+      type: String,
+    },
+    questions: [questionSchema],
+    finalScore: { type: Number, default: 0 },
+    status: {
+      type: String,
+      enum: ["Incompleted", "completed"],
+      default: "Incompleted",
+    },
+  },
+  { timestamps: true },
+);
+const Interview = mongoose.model("Interview", interviewSchema);
+export default Interview;
